5113f71e2e183b69dff20bd51d17f216564a4d80
Sandra <sandra.villot038@gmail.com>
2022-06-18 14:35:12 +0200

    ajout fixture et affichage des articles de la bdd

--- blog_symfony/src/Controller/BlogController.php
+++ blog_symfony/src/Controller/BlogController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="app_blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' =>$articles
         ]);
     }
 
@@ -27,10 +31,13 @@ class BlogController extends AbstractController
     }
     
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show()
+    public function show(ArticleRepository $repo, $id)
     {
-        return $this->render('blog/show.html.twig');
+        $article = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
